434f8b2a793a69ca652721ccf420c3182951718a
videah <videah@selfish.systems>
2019-02-06 19:23:33 +0000

    add bottom buttons

--- lib/pages/setup/confirm_page.dart
+++ lib/pages/setup/confirm_page.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:generic_bloc_provider/generic_bloc_provider.dart';
 import 'package:kaminari_wallet/blocs/confirm_bloc.dart';
 import 'package:kaminari_wallet/generated/protos/lnrpc.pbgrpc.dart';
+import 'package:kaminari_wallet/widgets/bottom_button_bar.dart';
+import 'package:kaminari_wallet/widgets/fill_icon_button.dart';
 
 class ConfirmPage extends StatelessWidget {
   @override
@@ -10,32 +12,67 @@ class ConfirmPage extends StatelessWidget {
       appBar: AppBar(
         title: Text("Setup"),
       ),
-      body: Column(
-        children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Text(
-              "We first need to connect and authenticate with your node.",
-              textAlign: TextAlign.center,
-            ),
-          ),
-          StreamBuilder(
-            stream: BlocProvider.of<ConfirmBloc>(context).info,
-            builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                return Expanded(
-                  child: ListView(
-                    children: <Widget>[],
+      body: StreamBuilder<Object>(
+        stream: BlocProvider.of<ConfirmBloc>(context).info,
+        builder: (context, snapshot) {
+          GetInfoResponse node = snapshot.data;
+          if (snapshot.hasData) {
+            return Column(
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Text(
+                    "Does this look right?",
+                    textAlign: TextAlign.center,
                   ),
-                );
-              } else {
-                return Center(
-                  child: CircularProgressIndicator(),
-                );
-              }
-            }
-          )
-        ],
+                ),
+                Expanded(
+                  child: ListView(
+                    children: <Widget>[
+                      ListTile(
+                        title: Text("Name"),
+                        subtitle: Text("${node.alias}"),
+                      ),
+                      ListTile(
+                        title: Text("Public Key"),
+                        subtitle: Text("${node.identityPubkey}"),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            );
+          } else {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        }
+      ),
+      bottomNavigationBar: StreamBuilder(
+        stream: BlocProvider.of<ConfirmBloc>(context).info,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return BottomButtonBar(
+              children: <Widget>[
+                FillIconButton(
+                  icon: Icon(Icons.close),
+                  backgroundColor: Colors.red,
+                  child: Text("No"),
+                  onTap: () {},
+                ),
+                FillIconButton(
+                  icon: Icon(Icons.check),
+                  backgroundColor: Colors.green,
+                  child: Text("Yes"),
+                  onTap: () {},
+                )
+              ],
+            );
+          } else {
+            return Container();
+          }
+        },
       ),
     );
   }
