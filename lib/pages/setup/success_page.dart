66226f5d35747721e7323d43955839341281568f
videah <videah@selfish.systems>
2019-02-06 21:18:56 +0000

    svg on success page

--- lib/pages/setup/success_page.dart
+++ lib/pages/setup/success_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:kaminari_wallet/widgets/bottom_button_bar.dart';
 import 'package:kaminari_wallet/widgets/fill_icon_button.dart';
+import 'package:flutter_svg/svg.dart';
 
 class SuccessPage extends StatelessWidget {
   @override
@@ -8,6 +9,23 @@ class SuccessPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text("Complete Setup"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              width: 200,
+              height: 200,
+              padding: EdgeInsets.all(16.0),
+              child: SvgPicture.asset("assets/svg/tada.svg"),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text("Hurray! Everything's good to go!"),
+            ),
+          ],
+        ),
       ),
       bottomNavigationBar: BottomButtonBar(
         children: <Widget>[
