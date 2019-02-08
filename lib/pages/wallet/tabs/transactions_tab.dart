33198fff4815339356446fd1ede18eacc0e155c1
videah <videah@selfish.systems>
2019-02-08 17:02:09 +0000

    add onrefresh

--- lib/pages/wallet/tabs/transactions_tab.dart
+++ lib/pages/wallet/tabs/transactions_tab.dart
@@ -2,23 +2,39 @@ import 'package:flutter/material.dart';
 import 'package:kaminari_wallet/widgets/wallet/transaction_tile.dart';
 
 class TransactionsTab extends StatelessWidget {
+
+  /// The [Transform] widgets are an ugly hack to make the [RefreshIndicator]
+  /// appear in the right place when using [SliverOverlapAbsorber]
+  /// and [SliverOverlapInjector] to fix the sliver layout.
+
   @override
   Widget build(BuildContext context) {
-    return CustomScrollView(
-      slivers: <Widget>[
-        SliverOverlapInjector(
-          handle: NestedScrollView.sliverOverlapAbsorberHandleFor(context),
-        ),
-        SliverPadding(
-          padding: EdgeInsets.all(0.0),
-          sliver: SliverList(
-            delegate: SliverChildListDelegate(
-              [
-              ],
-            ),
+    var overlapAbsorber =
+        NestedScrollView.sliverOverlapAbsorberHandleFor(context);
+    return Transform.translate(
+      offset: Offset(0.0, overlapAbsorber.layoutExtent),
+      child: RefreshIndicator(
+        onRefresh: () async {},
+        child: Transform.translate(
+          offset: Offset(0.0, -overlapAbsorber.layoutExtent),
+          child: CustomScrollView(
+            slivers: <Widget>[
+              SliverOverlapInjector(handle: overlapAbsorber),
+              SliverPadding(
+                padding: EdgeInsets.all(0.0),
+                sliver: SliverList(
+                  delegate: SliverChildBuilderDelegate(
+                    (context, index) {
+                      return Container();
+                    },
+                    childCount: 4,
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
-      ],
+      ),
     );
   }
 }
