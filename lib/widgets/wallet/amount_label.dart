d28ce96d097bade0418f972b54ef37548b3ecdce
videah <videah@selfish.systems>
2019-02-08 19:29:30 +0000

    move label to its own widget

---
+++ lib/widgets/wallet/amount_label.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:kaminari_wallet/widgets/wallet/transaction_tile.dart';
+
+class AmountLabel extends StatelessWidget {
+  final String text;
+  final TxDirection direction;
+
+  const AmountLabel({Key key, this.text, this.direction}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    var icon = direction == TxDirection.receiving
+        ? FontAwesomeIcons.plus
+        : FontAwesomeIcons.minus;
+
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.end,
+      crossAxisAlignment: CrossAxisAlignment.end,
+      children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.all(4.0),
+          child: Icon(
+            icon,
+            color: Colors.green,
+            size: 12.0,
+          ),
+        ),
+        RichText(
+          text: TextSpan(
+            text: text,
+            style: Theme.of(context)
+                .textTheme
+                .body1
+                .copyWith(fontSize: 20.0, fontWeight: FontWeight.bold),
+            children: [
+              TextSpan(text: " sat", style: TextStyle(fontSize: 13.0)),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
--- lib/widgets/wallet/transaction_tile.dart
+++ lib/widgets/wallet/transaction_tile.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:kaminari_wallet/widgets/wallet/amount_label.dart';
 
 enum TxDirection {sending, receiving}
 
@@ -16,28 +17,9 @@ class TransactionTile extends StatelessWidget {
     return ListTile(
       title: title ?? Text("Unknown Payment"),
       subtitle: subtitle ?? null,
-      trailing: Row(
-        mainAxisSize: MainAxisSize.min,
-        mainAxisAlignment: MainAxisAlignment.end,
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: <Widget>[
-          direction == TxDirection.receiving ? Padding(
-            padding: const EdgeInsets.all(4.0),
-            child: Icon(FontAwesomeIcons.plus, color: Colors.green, size: 12.0,),
-          ) : direction == TxDirection.sending ? Padding(
-            padding: const EdgeInsets.all(4.0),
-            child: Icon(FontAwesomeIcons.minus, color: Colors.red, size: 12.0,),
-          ) : Container(),
-          RichText(
-            text: TextSpan(
-              text: "1337",
-              style: Theme.of(context).textTheme.body1.copyWith(fontSize: 20.0, fontWeight: FontWeight.bold),
-              children: [
-                TextSpan(text: " sat", style: TextStyle(fontSize: 13.0)),
-              ]
-            )
-          ),
-        ],
+      trailing: AmountLabel(
+        text: "1337",
+        direction: direction,
       ),
       leading: CircleAvatar(
         backgroundImage: image ?? null,
