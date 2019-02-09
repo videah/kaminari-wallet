5fe2d42912a93b49d849886b0a73f7432e6f3800
videah <videah@selfish.systems>
2019-02-09 14:31:42 +0000

    combine payments and transactions

--- lib/blocs/main_wallet_bloc.dart
+++ lib/blocs/main_wallet_bloc.dart
@@ -5,12 +5,17 @@ import 'package:rxdart/rxdart.dart';
 class MainWalletBloc extends LightningBloc {
   String _balance;
   List<Transaction> _transactions;
+  List<Payment> _payments;
+  List<dynamic> _history = [];
 
   final _balanceSubject = BehaviorSubject<String>(seedValue: "0 sat");
   Stream get balance => _balanceSubject.stream;
 
   final _transactionSubject = BehaviorSubject<List<Transaction>>();
   Stream get transactions => _transactionSubject.stream;
+
+  final _historySubject = BehaviorSubject<List<dynamic>>();
+  Stream get history => _historySubject.stream;
 
   MainWalletBloc() {
     _setup();
@@ -19,7 +24,9 @@ class MainWalletBloc extends LightningBloc {
   void _setup() async {
     await lightning.initialize();
     _syncBalance();
-    _syncTransactions();
+    await _syncTransactions();
+    await _syncPayments();
+    await _sortHistory();
     lightning.client
         .subscribeTransactions(GetTransactionsRequest())
         .listen((tx) {
@@ -35,18 +42,39 @@ class MainWalletBloc extends LightningBloc {
     _balanceSubject.add("$_balance sat");
   }
 
-  void _syncTransactions() async {
+  Future _syncTransactions() async {
     var response = await lightning.client.getTransactions(
       GetTransactionsRequest(),
     );
-    _transactions = response.transactions;
-    _transactionSubject.add(_transactions.reversed.toList());
+    _transactions = response.transactions.toList();
+    _history.addAll(_transactions);
+    _transactionSubject.add(_transactions);
+  }
+
+  Future _syncPayments() async {
+    var response = await lightning.client.listPayments(ListPaymentsRequest());
+    _payments = response.payments.toList();
+    _history.addAll(_payments);
+  }
+
+  Future _sortHistory() async {
+    _history.sort((a, b) {
+      var aTimestamp = a is Transaction
+          ? a.timeStamp
+          : a is Invoice ? a.creationDate : a.creationDate;
+      var bTimestamp = b is Transaction
+          ? b.timeStamp
+          : b is Invoice ? b.creationDate : b.creationDate;
+      return aTimestamp < bTimestamp ? 1 : aTimestamp > bTimestamp ? -1 : 0;
+    });
+    _historySubject.add(_history);
   }
 
   @override
   void dispose() {
     _balanceSubject.close();
     _transactionSubject.close();
+    _historySubject.close();
     lightning.client.closeChannel(CloseChannelRequest());
   }
 }
--- lib/pages/wallet/tabs/transactions_tab.dart
+++ lib/pages/wallet/tabs/transactions_tab.dart
@@ -20,8 +20,8 @@ class TransactionsTab extends StatelessWidget {
         onRefresh: () async {},
         child: Transform.translate(
           offset: Offset(0.0, -overlapAbsorber.layoutExtent),
-          child: StreamBuilder<List<Transaction>>(
-            stream: bloc.transactions,
+          child: StreamBuilder<List<dynamic>>(
+            stream: bloc.history,
             builder: (context, snapshot) {
               if (snapshot.hasData) {
                 return CustomScrollView(
@@ -31,19 +31,35 @@ class TransactionsTab extends StatelessWidget {
                       padding: EdgeInsets.all(0.0),
                       sliver: SliverList(
                         delegate: SliverChildBuilderDelegate((context, index) {
-                          var tx = snapshot.data[index];
-                          return TransactionTile(
-                            title: Text(
-                              "Anonymous",
-                              style: TextStyle(fontWeight: FontWeight.bold),
-                            ),
-                            subtitle: Text("Chain Transaction"),
-                            amount: tx.amount.toInt(),
-                            direction: tx.amount < 0 ? TxDirection.sending : TxDirection.receiving,
-                            image: NetworkImage(
-                              "https://pbs.twimg.com/profile_images/941678006606729217/C4L6sQEf_400x400.jpg",
-                            ),
-                          );
+                          if (snapshot.data[index] is Transaction) {
+                            Transaction tx = snapshot.data[index];
+                            return TransactionTile(
+                              title: Text(
+                                "Anonymous",
+                                style: TextStyle(fontWeight: FontWeight.bold),
+                              ),
+                              subtitle: Text("Chain Transaction"),
+                              amount: tx.amount.toInt(),
+                              direction: tx.amount < 0 ? TxDirection.sending : TxDirection.receiving,
+                              image: NetworkImage(
+                                "https://pbs.twimg.com/profile_images/941678006606729217/C4L6sQEf_400x400.jpg",
+                              ),
+                            );
+                          } else if (snapshot.data[index] is Payment) {
+                            Payment tx = snapshot.data[index];
+                            return TransactionTile(
+                              title: Text(
+                                "Anonymous",
+                                style: TextStyle(fontWeight: FontWeight.bold),
+                              ),
+                              subtitle: Text("${tx.paymentHash}"),
+                              amount: tx.value.toInt(),
+                              direction: TxDirection.sending,
+                              image: NetworkImage(
+                                "https://pbs.twimg.com/profile_images/941678006606729217/C4L6sQEf_400x400.jpg",
+                              ),
+                            );
+                          }
                         }, childCount: snapshot.data.length),
                       ),
                     ),
