e87cad1d796ec54c9e3a8a3c81d45ce2a77d5fdb
videah <videah@selfish.systems>
2019-06-30 01:44:43 +0100

    working on new scan page

--- lib/pages/wallet/scan_invoice_page.dart
+++ lib/pages/wallet/scan_invoice_page.dart
@@ -1,14 +1,116 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_qr_reader/flutter_qr_reader.dart';
+import 'package:flutter/services.dart';
+import 'package:generic_bloc_provider/generic_bloc_provider.dart';
+import 'package:kaminari_wallet/blocs/send_bloc.dart';
+import 'package:kaminari_wallet/pages/wallet/send_page.dart';
+import 'package:qr_code_scanner/qr_code_scanner.dart';
 
 class ScanInvoicePage extends StatelessWidget {
+  final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
+  QRViewController _controller;
+  bool _alreadyRecognized = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Hello World"),
+        title: Text("Scan Invoice"),
+        elevation: 0.0,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.rotate_left),
+            onPressed: () {
+              _controller.flipCamera();
+            },
+          ),
+        ],
       ),
-      body: QrReaderView(),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: Stack(
+              children: <Widget>[
+                QRView(
+                  onQRViewCreated: (controller) {
+                    final channel = controller.channel;
+                    controller.init(qrKey);
+                    _controller = controller;
+                    channel.setMethodCallHandler((MethodCall call) async {
+                      switch (call.method) {
+                        case "onRecognizeQR":
+                          dynamic arguments = call.arguments;
+                          Uri uri = Uri.parse(arguments.toString());
+                          String invoice = arguments
+                              .toString()
+                              .substring(uri.scheme.length + 1);
+                          if (!_alreadyRecognized) {
+                            _alreadyRecognized = true;
+                            Navigator.of(context).pushReplacement(
+                              MaterialPageRoute(
+                                builder: (context) => BlocProvider(
+                                  bloc: SendBloc("$invoice"),
+                                  child: SendPage(),
+                                ),
+                              ),
+                            );
+                          }
+                      }
+                    });
+                  },
+                ),
+                ClipPath(
+                  clipper: CustomRect(),
+                  child: Container(
+                    color: Colors.deepPurple,
+                    child: Center(
+                      child: Hero(
+                        tag: "test-trans",
+                        child: Container(
+                          color: Colors.transparent,
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(16),
+                            child: Container(
+                              color: Colors.white,
+                              width: 315,
+                              height: 315,
+                              child: Center(
+                                child: CircularProgressIndicator(),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
     );
+  }
+}
+
+class CustomRect extends CustomClipper<Path> {
+  Path getClip(Size size) {
+    int radius = 150;
+    return Path()
+      ..addRRect(
+        RRect.fromLTRBR(
+          (size.width / 2) - radius,
+          (size.height / 2) - radius,
+          (size.width / 2) + radius,
+          (size.height / 2) + radius,
+          Radius.circular(16),
+        ),
+      )
+      ..addRect(Rect.fromLTWH(0.0, 0.0, size.width, size.height))
+      ..fillType = PathFillType.evenOdd;
+  }
+
+  @override
+  bool shouldReclip(CustomRect oldClipper) {
+    return true;
   }
 }
--- lib/pages/wallet/send_page.dart
+++ lib/pages/wallet/send_page.dart
@@ -57,175 +57,178 @@ class SendPageState extends State<SendPage> with TickerProviderStateMixin {
     var bloc = BlocProvider.of<SendBloc>(context);
     return Stack(
       children: <Widget>[
-        Scaffold(
-          appBar: AppBar(
-            title: Text("Send Payment"),
-          ),
-          body: StreamBuilder<PayReq>(
-            stream: bloc.request,
-            builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                return Column(
-                  children: <Widget>[
-                    Padding(
-                      padding: const EdgeInsets.only(top: 16.0, bottom: 8.0),
-                      child: DefaultTextStyle(
-                        style: TextStyle(
-                          color: DefaultTextStyle.of(context).style.color,
-                          fontSize: 18,
-                        ),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: <Widget>[
-                            Container(
-                              width: 180,
-                              child: StreamBuilder<GetInfoResponse>(
-                                stream: bloc.node,
-                                builder: (context, snapshot) {
-                                  if (snapshot.hasData) {
-                                    return Column(
-                                      children: <Widget>[
-                                        Padding(
-                                          padding: const EdgeInsets.only(
-                                              bottom: 8.0),
-                                          child: RoundedIdenticon(
-                                            "${snapshot.data.identityPubkey}",
-                                            scale: 80,
+        Hero(
+          tag: "test-trans",
+          child: Scaffold(
+            appBar: AppBar(
+              title: Text("Send Payment"),
+            ),
+            body: StreamBuilder<PayReq>(
+              stream: bloc.request,
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  return Column(
+                    children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.only(top: 16.0, bottom: 8.0),
+                        child: DefaultTextStyle(
+                          style: TextStyle(
+                            color: DefaultTextStyle.of(context).style.color,
+                            fontSize: 18,
+                          ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: <Widget>[
+                              Container(
+                                width: 180,
+                                child: StreamBuilder<GetInfoResponse>(
+                                  stream: bloc.node,
+                                  builder: (context, snapshot) {
+                                    if (snapshot.hasData) {
+                                      return Column(
+                                        children: <Widget>[
+                                          Padding(
+                                            padding: const EdgeInsets.only(
+                                                bottom: 8.0),
+                                            child: RoundedIdenticon(
+                                              "${snapshot.data.identityPubkey}",
+                                              scale: 80,
+                                            ),
                                           ),
-                                        ),
-                                        Text("${snapshot.data.alias}")
-                                      ],
-                                    );
-                                  } else {
-                                    return Center(
-                                      child: CircularProgressIndicator(),
-                                    );
-                                  }
-                                },
+                                          Text("${snapshot.data.alias}")
+                                        ],
+                                      );
+                                    } else {
+                                      return Center(
+                                        child: CircularProgressIndicator(),
+                                      );
+                                    }
+                                  },
+                                ),
                               ),
-                            ),
-                            Icon(
-                              FontAwesomeIcons.longArrowAltRight,
-                              size: 32,
-                            ),
-                            Container(
-                              width: 180,
-                              child: Column(
-                                children: <Widget>[
-                                  Padding(
-                                    padding: const EdgeInsets.only(bottom: 8.0),
-                                    child: RoundedIdenticon(
-                                      "${snapshot.data.destination}",
-                                      scale: 80,
+                              Icon(
+                                FontAwesomeIcons.longArrowAltRight,
+                                size: 32,
+                              ),
+                              Container(
+                                width: 180,
+                                child: Column(
+                                  children: <Widget>[
+                                    Padding(
+                                      padding: const EdgeInsets.only(bottom: 8.0),
+                                      child: RoundedIdenticon(
+                                        "${snapshot.data.destination}",
+                                        scale: 80,
+                                      ),
                                     ),
-                                  ),
-                                  StreamBuilder<NodeInfo>(
-                                    stream: bloc.destination,
-                                    builder: (context, snapshot) {
-                                      if (snapshot.hasError) {
-                                        return Text(
-                                          "Unknown Node",
-                                          style: TextStyle(
-                                            color: Colors.deepPurple,
-                                            fontStyle: FontStyle.italic,
-                                          ),
-                                        );
-                                      }
-                                      if (snapshot.hasData) {
-                                        return Text(
-                                            "${snapshot.data.node.alias}");
-                                      } else {
-                                        return Text("");
-                                      }
-                                    },
+                                    StreamBuilder<NodeInfo>(
+                                      stream: bloc.destination,
+                                      builder: (context, snapshot) {
+                                        if (snapshot.hasError) {
+                                          return Text(
+                                            "Unknown Node",
+                                            style: TextStyle(
+                                              color: Colors.deepPurple,
+                                              fontStyle: FontStyle.italic,
+                                            ),
+                                          );
+                                        }
+                                        if (snapshot.hasData) {
+                                          return Text(
+                                              "${snapshot.data.node.alias}");
+                                        } else {
+                                          return Text("");
+                                        }
+                                      },
+                                    )
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      Divider(
+                        height: 0.0,
+                      ),
+                      Expanded(
+                        child: SeperatedListView(
+                          divider: Divider(
+                            height: 0.0,
+                          ),
+                          children: <Widget>[
+                            ListTile(
+                              title: Text("Amount"),
+                              subtitle: Text("${snapshot.data.numSatoshis} sat"),
+                            ),
+                            snapshot.data.description != ""
+                                ? ListTile(
+                                    title: Text("Description"),
+                                    subtitle:
+                                        Text("${snapshot.data.description}"),
                                   )
-                                ],
+                                : null,
+                            ListTile(
+                              title: Text("Invoice Date"),
+                              subtitle: Text(
+                                DateFormat.yMEd()
+                                    .format(
+                                      DateTime.fromMillisecondsSinceEpoch(
+                                        snapshot.data.timestamp.toInt() * 1000,
+                                      ),
+                                    )
+                                    .toString(),
                               ),
                             ),
                           ],
                         ),
-                      ),
-                    ),
-                    Divider(
-                      height: 0.0,
-                    ),
-                    Expanded(
-                      child: SeperatedListView(
-                        divider: Divider(
-                          height: 0.0,
-                        ),
-                        children: <Widget>[
-                          ListTile(
-                            title: Text("Amount"),
-                            subtitle: Text("${snapshot.data.numSatoshis} sat"),
-                          ),
-                          snapshot.data.description != ""
-                              ? ListTile(
-                                  title: Text("Description"),
-                                  subtitle:
-                                      Text("${snapshot.data.description}"),
-                                )
-                              : null,
-                          ListTile(
-                            title: Text("Invoice Date"),
-                            subtitle: Text(
-                              DateFormat.yMEd()
-                                  .format(
-                                    DateTime.fromMillisecondsSinceEpoch(
-                                      snapshot.data.timestamp.toInt() * 1000,
-                                    ),
-                                  )
-                                  .toString(),
-                            ),
-                          ),
-                        ],
-                      ),
-                    )
-                  ],
-                );
-              } else {
-                return Center(
-                  child: CircularProgressIndicator(),
-                );
-              }
-            },
-          ),
-          bottomNavigationBar: BottomButtonBar(
-            children: <Widget>[
-              StreamBuilder<PayReq>(
-                stream: bloc.request,
-                builder: (context, snapshot) {
-                  if (snapshot.hasData) {
-                    return StreamBuilder<Object>(
-                      stream: bloc.attempting,
-                      builder: (context, attempt) {
-                        if (attempt.hasData && attempt.data) {
-                          return FillIconButton(
-                            child: CircularProgressIndicator(
-                              valueColor: AlwaysStoppedAnimation<Color>(
-                                Colors.white,
+                      )
+                    ],
+                  );
+                } else {
+                  return Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+              },
+            ),
+            bottomNavigationBar: BottomButtonBar(
+              children: <Widget>[
+                StreamBuilder<PayReq>(
+                  stream: bloc.request,
+                  builder: (context, snapshot) {
+                    if (snapshot.hasData) {
+                      return StreamBuilder<Object>(
+                        stream: bloc.attempting,
+                        builder: (context, attempt) {
+                          if (attempt.hasData && attempt.data) {
+                            return FillIconButton(
+                              child: CircularProgressIndicator(
+                                valueColor: AlwaysStoppedAnimation<Color>(
+                                  Colors.white,
+                                ),
                               ),
-                            ),
 //                        onTap: () {},
-                          );
-                        } else {
-                          return FillIconButton(
-                            child:
-                                Text("Send ${snapshot.data.numSatoshis} sat"),
-                            icon: Icon(FontAwesomeIcons.bolt),
-                            onTap: () {
-                              bloc.startAttempt.add(true);
-                            },
-                          );
-                        }
-                      },
-                    );
-                  } else {
-                    return Container();
-                  }
-                },
-              ),
-            ],
+                            );
+                          } else {
+                            return FillIconButton(
+                              child:
+                                  Text("Send ${snapshot.data.numSatoshis} sat"),
+                              icon: Icon(FontAwesomeIcons.bolt),
+                              onTap: () {
+                                bloc.startAttempt.add(true);
+                              },
+                            );
+                          }
+                        },
+                      );
+                    } else {
+                      return Container();
+                    }
+                  },
+                ),
+              ],
+            ),
           ),
         ),
         StreamBuilder<SendResponse>(
